f4673774af51285750d244e57e632d9bbc955f7f
siar1234 <siar019283@gmail.com>
2025-06-18 11:04:29 +0900

    chore: remove unnecessary print

--- app/src/main/kotlin/com/amphi/App.kt
+++ app/src/main/kotlin/com/amphi/App.kt
@@ -45,7 +45,7 @@ class App : AbstractVerticle(), Handler<HttpServerRequest> {
         vertx.createHttpServer().requestHandler{req ->
 
             val ipAddress = req.remoteAddress().hostAddress()
-            println("request - ip: $ipAddress, ${req.path()} ${req.method().name()}")
+//            println("request - ip: $ipAddress, ${req.path()} ${req.method().name()}")
             if(RateLimiter.isAllowed(ipAddress)) {
                 if(ServerSettings.whitelistOnly) {
                     if(ServerSettings.inWhiteList(ipAddress)) {
