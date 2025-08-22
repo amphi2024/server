d01fa34b4daa66ac08628371f28906f58834ed19
siar1234 <siar019283@gmail.com>
2025-08-22 16:36:08 +0900

    fix: calculate duration using ZonedDateTime to handle time zone correctly

--- app/src/main/kotlin/com/amphi/ServerFileUtils.kt
+++ app/src/main/kotlin/com/amphi/ServerFileUtils.kt
@@ -6,6 +6,7 @@ import com.amphi.models.TrashLog
 import java.io.File
 import java.time.Duration
 import java.time.LocalDateTime
+import java.time.ZoneId
 
 object ServerFileUtils {
 
@@ -108,7 +109,10 @@ object ServerFileUtils {
             if(file.isFile) {
                 trashLogs.forEach { trashLog ->
                     if(trashLog.path == file.path) {
-                        val period = Duration.between(trashLog.timeStamp, LocalDateTime.now())
+                        val period = Duration.between(
+                            trashLog.timeStamp.atZone(ZoneId.systemDefault()).toInstant(),
+                            LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant()
+                        )
                         if (period.toDays() > 30) {
                             file.delete()
                             ServerDatabase.deleteTrashLog(trashLog.path)
