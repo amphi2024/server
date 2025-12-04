331dd9c687988ea32a8d32acca31eb0c50417aba
siar1234 <siar019283@gmail.com>
2025-12-04 12:45:33 +0900

    feat: add photo and album fetching and deletion

--- src/main/kotlin/com/amphi/server/models/photos/PhotosDatabase.kt
+++ src/main/kotlin/com/amphi/server/models/photos/PhotosDatabase.kt
@@ -4,6 +4,8 @@ import com.amphi.server.utils.setNullable
 import java.sql.DriverManager
 import java.sql.PreparedStatement
 import java.sql.Types
+import java.time.Duration
+import java.time.Instant
 
 class PhotosDatabase(val userId: String) {
 
@@ -74,6 +76,73 @@ class PhotosDatabase(val userId: String) {
         }
     }
 
+    fun getPhotos(): List<Photo> {
+        val sql = "SELECT * FROM photos WHERE permanently_deleted IS NULL;"
+        val list = mutableListOf<Photo>()
+        val statement = connection.createStatement()
+        val resultSet = statement.executeQuery(sql)
+        while (resultSet.next()) {
+            val photos = Photo.fromResultSet(resultSet)
+            list.add(photos)
+        }
+
+        resultSet.close()
+        statement.close()
+
+        return list
+    }
+
+    fun getAlbums(): List<Album> {
+        val sql = "SELECT * FROM albums WHERE permanently_deleted IS NULL;"
+        val list = mutableListOf<Album>()
+        val statement = connection.createStatement()
+        val resultSet = statement.executeQuery(sql)
+        while (resultSet.next()) {
+            val album = Album.fromResultSet(resultSet)
+            list.add(album)
+        }
+
+        resultSet.close()
+        statement.close()
+
+        return list
+    }
+
+
+    fun getPhotoById(id: String): Photo? {
+        val sql = "SELECT * FROM photos WHERE id = ?;"
+        val statement = connection.prepareStatement(sql)
+        statement.setString(1, id)
+        val resultSet = statement.executeQuery()
+        if (resultSet.next()) {
+            val photo = Photo.fromResultSet(resultSet)
+            resultSet.close()
+            statement.close()
+            return photo
+        } else {
+            resultSet.close()
+            statement.close()
+            return null
+        }
+    }
+
+    fun getAlbumById(id: String): Album? {
+        val sql = "SELECT * FROM albums WHERE id = ?;"
+        val statement = connection.prepareStatement(sql)
+        statement.setString(1, id)
+        val resultSet = statement.executeQuery()
+        if (resultSet.next()) {
+            val album = Album.fromResultSet(resultSet)
+            resultSet.close()
+            statement.close()
+            return album
+        } else {
+            resultSet.close()
+            statement.close()
+            return null
+        }
+    }
+
     fun insertPhoto(photo: Photo, onComplete: ((result: Int) -> Unit)? = null) {
         val sql = """
                 INSERT INTO photos (
@@ -92,7 +161,8 @@ class PhotosDatabase(val userId: String) {
                     mime_type = excluded.mime_type,
                     sha256 = excluded.sha256,
                     note = excluded.note,
-                    tags = excluded.tags;
+                    tags = excluded.tags,
+                    permanently_deleted = NULL;
                 """.trimIndent()
         val preparedStatement = connection.prepareStatement(sql)
         preparedStatement.setPhoto(photo)
@@ -130,7 +200,8 @@ class PhotosDatabase(val userId: String) {
                     deleted = excluded.deleted,
                     photos = excluded.photos,
                     cover_photo_index = excluded.cover_photo_index,
-                    note = excluded.note;
+                    note = excluded.note,
+                    permanently_deleted = NULL;
                 """.trimIndent()
         val preparedStatement = connection.prepareStatement(sql)
         preparedStatement.setAlbum(album)
@@ -148,5 +219,117 @@ class PhotosDatabase(val userId: String) {
         setString(6, album.photos.toString())
         setNullable(7, album.coverPhotoIndex, Types.INTEGER)
         setNullable(8, album.note, Types.VARCHAR)
+    }
+
+//    fun getThemes(): List<PhotosTheme> {
+//        val sql = "SELECT * FROM themes;"
+//        val list = mutableListOf<PhotosTheme>()
+//        val statement = connection.createStatement()
+//        val resultSet = statement.executeQuery(sql)
+//        while (resultSet.next()) {
+//            list.add(PhotosTheme.fromResultSet(resultSet))
+//        }
+//
+//        resultSet.close()
+//        statement.close()
+//
+//        return list
+//    }
+//
+//    fun insertTheme(theme: PhotosTheme) {
+//        val sql = """
+//                INSERT INTO themes (
+//                    id, title, created, modified, background_light, text_light, accent_light,
+//                    card_light, background_dark, text_dark,
+//                    accent_dark, card_dark
+//                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+//                    ON CONFLICT(id) DO UPDATE SET
+//                  title = excluded.title,
+//                  created = excluded.created,
+//                  modified = excluded.modified,
+//                  background_light = excluded.background_light,
+//                  text_light = excluded.text_light,
+//                  accent_light = excluded.accent_light,
+//                  card_light = excluded.card_light,
+//                  background_dark = excluded.background_dark,
+//                  text_dark = excluded.text_dark,
+//                  accent_dark = excluded.accent_dark,
+//                  card_dark = excluded.card_dark;
+//                """.trimIndent()
+//        val statement = connection.prepareStatement(sql)
+//        statement.setTheme(theme)
+//        statement.executeUpdate()
+//        statement.close()
+//    }
+//
+//    private fun PreparedStatement.setTheme(theme: PhotosTheme) {
+//        setString(1, theme.id)
+//        setString(2, theme.title)
+//        setLong(3, theme.created)
+//        setLong(4, theme.modified)
+//        setLong(5, theme.lightColors.background)
+//        setLong(6, theme.lightColors.text)
+//        setLong(7, theme.lightColors.accent)
+//        setLong(8, theme.lightColors.card)
+//        setLong(9, theme.darkColors.background)
+//        setLong(10, theme.darkColors.text)
+//        setLong(11, theme.darkColors.accent)
+//        setLong(12, theme.darkColors.card)
+//    }
+//
+//    fun getThemeById(id: String): PhotosTheme? {
+//        val sql = "SELECT * FROM themes WHERE id = ?;"
+//        val statement = connection.prepareStatement(sql)
+//        statement.setString(1, id)
+//        val resultSet = statement.executeQuery()
+//        if (resultSet.next()) {
+//            val theme = PhotosTheme.fromResultSet(resultSet)
+//            resultSet.close()
+//            statement.close()
+//            return theme
+//        } else {
+//            resultSet.close()
+//            statement.close()
+//            return null
+//        }
+//    }
+//
+//    fun deleteTheme(id: String) {
+//        val sql = "DELETE FROM themes WHERE id = ?;"
+//        val preparedStatement = connection.prepareStatement(sql)
+//        preparedStatement.setString(1, id)
+//        preparedStatement.executeUpdate()
+//        preparedStatement.close()
+//    }
+
+    fun setPhotoDeleted(id: String) = setItemDeleted("photos", id)
+    fun setAlbumDeleted(id: String) = setItemDeleted("albums", id)
+
+    private fun setItemDeleted(table: String, id: String) {
+        val sql = "UPDATE $table SET permanently_deleted = ? WHERE id = ?;"
+        connection.prepareStatement(sql).use { stmt ->
+            stmt.setLong(1, Instant.now().toEpochMilli())
+            stmt.setString(2, id)
+            stmt.executeUpdate()
+        }
+    }
+
+    fun deleteObsoleteItems() {
+        deleteObsoleteItemsForTable("photos")
+        deleteObsoleteItemsForTable("albums")
+    }
+
+    private fun deleteObsoleteItemsForTable(table: String) {
+        val threshold = Instant.now().minus(Duration.ofDays(30)).toEpochMilli()
+        val sql = """
+        DELETE FROM $table
+        WHERE permanently_deleted IS NOT NULL
+          AND permanently_deleted < ?
+        """.trimIndent()
+
+        connection.prepareStatement(sql).use { stmt ->
+            stmt.setLong(1, threshold)
+            stmt.executeUpdate()
+        }
     }
 }
