2fe4cce9539d448736e77fbab882ce8cc8b2d822
siar1234 <siar019283@gmail.com>
2026-04-21 09:59:58 +0900

    refactor: migrate shared authorization logic to non-blocking patterns

--- src/main/kotlin/com/amphi/server/common/authorization.kt
+++ src/main/kotlin/com/amphi/server/common/authorization.kt
@@ -4,25 +4,22 @@ import com.amphi.server.authorizationService
 import com.amphi.server.logger
 import com.amphi.server.models.Token
 import io.vertx.core.http.HttpServerRequest
-import java.time.Instant
 
-fun handleAuthorization(
-    req: HttpServerRequest,
-    onAuthenticated: (token: Token) -> Unit
-) {
-    val requestToken = req.headers()["Authorization"]
+fun HttpServerRequest.withAuth(onAuthenticated: (token: Token) -> Unit) {
+    val requestToken = this.headers()["Authorization"]
 
     if (requestToken.isNullOrBlank()) {
-        logger?.warn("[SECURITY] Authorization Missing: IP=${req.remoteAddress().hostAddress()}, Path=${req.path()}")
-        sendAuthFailed(req)
-    } else {
-        authorizationService.authenticateByToken(
-            token = requestToken,
-            onFailed = {
-                logger?.warn("[SECURITY] Authorization Failed: IP=${req.remoteAddress().hostAddress()}, Path=${req.path()}")
-                sendAuthFailed(req)
-            },
-            onAuthenticated = onAuthenticated
-        )
+        logger?.warn("[SECURITY] Authorization Missing: IP=${this.remoteAddress().hostAddress()}")
+        sendAuthFailed(this)
+        return
+    }
+
+    authorizationService.authenticateByToken(
+        token = requestToken,
+    ).onSuccess {
+      onAuthenticated(it)
+    }.onFailure {
+        logger?.warn("[SECURITY] Authorization Failed: IP=${this.remoteAddress().hostAddress()}, Path=${this.path()}")
+        sendAuthFailed(this)
     }
 }
