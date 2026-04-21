71394011a7efcdbe0a1f5d076462ab597986a6a9
siar1234 <siar019283@gmail.com>
2026-04-21 10:11:20 +0900

    refactor: update UserHandler to reflect UserService changes

--- src/main/kotlin/com/amphi/server/handlers/UserHandler.kt
+++ src/main/kotlin/com/amphi/server/handlers/UserHandler.kt
@@ -20,18 +20,27 @@ object UserHandler {
                 val password = jsonBody.getString("password")
 
                 if (id.isNullOrBlank() || name.isNullOrBlank() || password.isNullOrBlank()) {
-                    logger?.warn("[SECURITY] Register Rejected (Missing Fields): IP=${req.remoteAddress().hostAddress()}}")
+                    logger?.warn(
+                        "[SECURITY] Register Rejected (Missing Fields): IP=${
+                            req.remoteAddress().hostAddress()
+                        }}"
+                    )
                     sendAuthFailed(req)
                 } else {
-                    if(AppConfig.security.allowUserRegistration) {
-                        userService.register(id = id, name = name, password = password, onFailed = {
-                            req.response().setStatusCode(StatusCode.BAD_REQUEST).end(Messages.ID_ALREADY_TAKEN)
-                        }, onSuccess = {
-                            sendSuccess(req)
-                        })
-                    }
-                    else {
-                        logger?.warn("[SECURITY] Register Attempt Blocked (Registration Disabled): IP=${req.remoteAddress().hostAddress()}}")
+                    if (AppConfig.security.allowUserRegistration) {
+                        userService.register(id = id, name = name, password = password)
+                            .onSuccess {
+                                sendSuccess(req)
+                            }
+                            .onFailure {
+                                req.response().setStatusCode(StatusCode.BAD_REQUEST).end(Messages.ID_ALREADY_TAKEN)
+                            }
+                    } else {
+                        logger?.warn(
+                            "[SECURITY] Register Attempt Blocked (Registration Disabled): IP=${
+                                req.remoteAddress().hostAddress()
+                            }}"
+                        )
                         req.response().setStatusCode(StatusCode.FORBIDDEN).end(Messages.NOT_ALLOWED_REGISTER)
                     }
                 }
@@ -49,7 +58,6 @@ object UserHandler {
                 val password = jsonBody.getString("password")
                 val deviceName = req.headers().get("Device-Name")
 
-
                 if (id.isNullOrBlank() || deviceName.isNullOrBlank() || password.isNullOrBlank()) {
                     logger?.warn("[SECURITY] Login Rejected (Missing Fields): IP=${req.remoteAddress().hostAddress()}}")
                     sendAuthFailed(req)
@@ -58,31 +66,41 @@ object UserHandler {
                         id = id,
                         deviceName = deviceName,
                         password = password
-                    ) { isAuthenticated, token, username ->
-                        if (isAuthenticated) {
-                            logger?.info("[AUTH] Login Success: ID=$id, IP=${req.remoteAddress().hostAddress()}, Device=$deviceName")
-                            val response = req.response()
-                            response.putHeader("content-type", "text/plain")
-                            response.end("${token};${username}")
-                        } else {
-                            logger?.warn("[SECURITY] Login Failed (Invalid Credentials): ID=$id, IP=${req.remoteAddress().hostAddress()}}")
+                    ).onSuccess { result ->
+                        logger?.info(
+                            "[AUTH] Login Success: ID=$id, IP=${
+                                req.remoteAddress().hostAddress()
+                            }, Device=$deviceName"
+                        )
+                        val response = req.response()
+                        response.putHeader("content-type", "text/plain")
+                        response.end(result)
+                    }
+                        .onFailure { throwable ->
+                            logger?.warn(
+                                "[SECURITY] Login Failed: ID=$id, IP=${req.remoteAddress().hostAddress()}",
+                                throwable
+                            )
                             sendAuthFailed(req)
                         }
-                    }
                 }
             }
         }
     }
 
     fun logout(req: HttpServerRequest) {
-        handleAuthorization(req) {token ->
-            userService.logout(token.token)
-            sendSuccess(req)
+        req.withAuth { token ->
+            userService.logout(token.token).onSuccess {
+                sendSuccess(req)
+            }
+                .onFailure {
+                    sendInternalServerError(req)
+                }
         }
     }
 
     fun changeUsername(req: HttpServerRequest) {
-        handleAuthorization(req) { token ->
+        req.withAuth { token ->
             req.bodyHandler { buffer ->
                 val jsonBody = buffer.toJsonObject()
                 if (jsonBody == null) {
@@ -93,18 +111,28 @@ object UserHandler {
                     if (name.isNullOrBlank()) {
                         sendAuthFailed(req)
                     } else {
-                        logger?.info("[USER] Username Changed: ID=${token.userId}, NewName=$name")
                         userService.changeUsername(
                             id = token.userId,
                             name = name,
-                        )
-                        eventService.saveEvent(
-                            value = name,
-                            action = "rename_user",
-                            token = token,
-                            appType = null
-                        )
-                        sendSuccess(req)
+                        ).compose {
+                            eventService.saveEvent(
+                                value = name,
+                                action = "rename_user",
+                                token = token,
+                                appType = null
+                            )
+                        }
+                            .onSuccess {
+                                logger?.info("[USER] Username Changed: ID=${token.userId}, NewName=$name")
+                                sendSuccess(req)
+                            }
+                            .onFailure { throwable ->
+                                logger?.info(
+                                    "[USER] Username Change Failed: ID=${token.userId}, NewName=$name",
+                                    throwable
+                                )
+                                sendInternalServerError(req)
+                            }
                     }
                 }
             }
@@ -121,27 +149,38 @@ object UserHandler {
                 val password = jsonBody.getString("password")
                 val oldPassword = jsonBody.getString("old_password")
 
-                if (id.isNullOrBlank() ||password.isNullOrBlank() ||oldPassword.isNullOrBlank()) {
-                    logger?.warn("[SECURITY] Change Password Rejected (Missing Fields): IP=${req.remoteAddress().hostAddress()}")
+                if (id.isNullOrBlank() || password.isNullOrBlank() || oldPassword.isNullOrBlank()) {
+                    logger?.warn(
+                        "[SECURITY] Change Password Rejected (Missing Fields): IP=${
+                            req.remoteAddress().hostAddress()
+                        }"
+                    )
                     sendAuthFailed(req)
                 } else {
-                    if(id == userId) {
+                    if (id == userId) {
                         userService.changePassword(
                             id = id,
                             password = password,
-                            oldPassword = oldPassword,
-                            onAuthenticationFailed = {
-                                logger?.warn("[SECURITY] Change Password Failed (Invalid Old Password): ID=$id, IP=${req.remoteAddress().hostAddress()}")
-                                sendAuthFailed(req)
-                            },
-                            onSuccess = {
+                            oldPassword = oldPassword
+                        )
+                            .onSuccess {
                                 logger?.info("[AUTH] Password Changed: ID=$id")
                                 sendSuccess(req)
                             }
+                            .onFailure { throwable ->
+                                logger?.warn(
+                                    "[SECURITY] Change Password Failed: ID=$id, IP=${
+                                        req.remoteAddress().hostAddress()
+                                    }", throwable
+                                )
+                                sendAuthFailed(req)
+                            }
+                    } else {
+                        logger?.error(
+                            "[SECURITY] Unauthorized Change Password Attempt: TargetID=$id, ActorID=$userId, IP=${
+                                req.remoteAddress().hostAddress()
+                            }"
                         )
-                    }
-                    else {
-                        logger?.error("[SECURITY] Unauthorized Change Password Attempt: TargetID=$id, ActorID=$userId, IP=${req.remoteAddress().hostAddress()}")
                         sendAuthFailed(req)
                     }
                 }
@@ -151,8 +190,19 @@ object UserHandler {
 
     fun getUserIds(req: HttpServerRequest) {
         logger?.info("[ADMIN] User List Accessed: IP=${req.remoteAddress().hostAddress()}")
-        req.response().putHeader("content-type", "application/json; charset=UTF-8").end("[${userService.getUserIds().joinToString {
-            "\"$it\""
-        }}]")
+        userService.getUserIds().onSuccess { ids ->
+            req.response().putHeader("content-type", "application/json; charset=UTF-8").end(
+                "[${
+                    ids.joinToString {
+                        "\"$it\""
+                    }
+                }]"
+            )
+        }
+            .onFailure { throwable ->
+                logger?.error("[USER] Could not retrieve ID list", throwable)
+                sendInternalServerError(req)
+            }
+
     }
 }
